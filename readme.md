027ce63a06669f69474119f867547b6331849113
单应青 <462548187@qq.com>
2020-05-15 10:23:08 +0800

    1. 增加代码注释

--- readme.md
+++ readme.md
@@ -5,5 +5,5 @@ Python Flask订餐系统
 
 ##flask-sqlacodegen
 
-        flask-sqlacodegen 'mysql://root:123456@127.0.0.1/food_db' --outfile "common/models/model.py"  --flask
-        flask-sqlacodegen 'mysql://root:123456@127.0.0.1/food_db' --tables user --outfile "common/models/user.py"  --flask
+        flask-sqlacodegen 'mysql://root:123456@127.0.0.1/food_db?charset=utf8mb4' --outfile "common/models/model.py"  --flask
+        flask-sqlacodegen 'mysql://root:123456@127.0.0.1/food_db?charset=utf8mb4' --tables user --outfile "common/models/user.py"  --flask
--- www.py
+++ www.py
@@ -2,10 +2,15 @@
 from application import app
 
 """
-统一拦截器
+统一拦截器,处理和统一错误处理
 """
 from web.interceptors.AuthInterceptor import *
-from web.interceptors.ErrorInterceptor import error_404  # 404错误页面
+# 404错误页面
+from web.interceptors.ErrorInterceptor import *
+
+"""
+蓝图功能，对所有的url进行蓝图功能配置
+"""
 
 from web.controllers.index import route_index
 from web.controllers.user.User import route_user
@@ -17,7 +22,6 @@ from web.controllers.stat.Stat import route_stat
 from web.controllers.api import route_api
 
 from web.controllers.static import route_static
-
 
 app.register_blueprint(route_index, url_prefix="/")
 app.register_blueprint(route_user, url_prefix="/user")
