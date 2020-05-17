68ba5db6a78e5a388278bd1600a0d68040a9add8
单应青 <462548187@qq.com>
2020-05-17 15:59:18 +0800

    美餐管理 新增删除和恢复js

---
+++ web/static/js/food/index.js
@@ -0,0 +1,51 @@
+;
+let food_index_ops = {
+    init:function () {
+        this.eventBind();
+    },
+    eventBind:function () {
+        let that = this;
+        $('.wrap_search .search').click(function () {
+            $('.wrap_search').submit();
+        });
+        // 删除事件
+        $('.remove').click(function () {
+            that.ops('remove', $(this).attr('data'));
+        });
+        // 恢复事件
+        $('.recover').click(function () {
+            that.ops('recover', $(this).attr('data'));
+        });
+    },
+    ops:function (act, id) {
+        let callback = {
+            'ok':function () {
+                       $.ajax({
+                            url: common_ops.buildUrl('/food/ops'),
+                            type: 'POST',
+                            data: {
+                                act: act,
+                                id: id
+                            },
+                            dataType: 'json',
+                            success: function (res) {
+                                let callback = null;
+                                if(res.code == 200){
+                                    callback = function () {
+                                        window.location.reload();
+                                    }
+                                }
+                                common_ops.alert(res.msg, callback);
+                            }
+                        });
+            },
+            'cancel': null
+        };
+        common_ops.confirm((act === 'remove' ? '确定删除吗？': '确定恢复嘛？'), callback)
+
+    }
+};
+
+$(document).ready(function () {
+    food_index_ops.init();
+});
